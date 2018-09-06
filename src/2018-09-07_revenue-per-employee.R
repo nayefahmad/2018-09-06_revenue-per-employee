7ac113d4fc5d127258cd55f4fa662aa939acb4e7
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2018-09-06 16:49:35 -0700

    pull, clean wiki table data

--- src/2018-09-07_revenue-per-employee.R
+++ src/2018-09-07_revenue-per-employee.R
@@ -9,11 +9,13 @@
 library("tidyverse")
 library("here")
 library("rvest")
+library("purrr")
+library("stringr")
 
 # rm(list = ls())
 
-# rvest example: --------------
-page <- read_html("http://www.zillow.com/homes/for_sale/Greenwood-IN/fsba,fsbo,fore,cmsn_lt/house_type/52333_rid/39.638414,-86.011362,39.550714,-86.179419_rect/12_zm/0_mmm/")
+# 0) rvest example: --------------
+page <- read_html("https://www.zillow.com/homes/for_sale/Greenwood-IN/fsba,fsbo,fore,cmsn_lt/house_type/52333_rid/39.638414,-86.011362,39.550714,-86.179419_rect/12_zm/0_mmm/")
 
 houses <- page %>%
       html_nodes(".photo-cards li article")
@@ -26,3 +28,31 @@ address <- houses %>%
 
 address
 str(address)
+
+
+
+# 1) trying with wikipedia: -------------
+page.wiki <- read_html("https://en.wikipedia.org/wiki/List_of_largest_companies_by_revenue")
+
+df1.companies <- page.wiki %>% 
+      html_node(xpath = '//*[@id="mw-content-text"]/div/table') %>% 
+      html_table()
+
+str(df1.companies)
+
+
+# 2) cleaning data: -------------
+df2.clean <- df1.companies %>% 
+      set_names(tolower(names(.))) %>% 
+      select(-c(country,
+                ref, 
+                `revenue growth`)) %>% 
+      rename(revenue = `revenue(usd millions)`) %>% 
+      mutate(revenue = str_sub(revenue, 2)) %>% 
+      mutate(revenue = str_replace_all(revenue, ",", ""), 
+             employees = str_replace_all(employees, ",", "")) %>% 
+      mutate(revenue = as.numeric(revenue), 
+             employees = as.numeric(employees))
+
+str(df2.clean)
+

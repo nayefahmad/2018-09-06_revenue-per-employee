b78b303785b18f2cb3276943152b7b815b745bce
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2018-09-06 16:33:44 -0700

    rvest example from Hadley

--- src/2018-09-07_revenue-per-employee.R
+++ src/2018-09-07_revenue-per-employee.R
@@ -1,8 +1,10 @@
 
 
 #*****************************************************************
-
+# PLOTTING REVENUE VERSUS EMPLOYEES FOR LARGEST COMPANIES BY REVENUE 
 #*****************************************************************
+# 2018-09-07
+# Nayef Ahmad 
 
 library("tidyverse")
 library("here")
@@ -10,4 +12,17 @@ library("rvest")
 
 # rm(list = ls())
 
+# rvest example: --------------
+page <- read_html("http://www.zillow.com/homes/for_sale/Greenwood-IN/fsba,fsbo,fore,cmsn_lt/house_type/52333_rid/39.638414,-86.011362,39.550714,-86.179419_rect/12_zm/0_mmm/")
 
+houses <- page %>%
+      html_nodes(".photo-cards li article")
+
+z_id <- houses %>% html_attr("id")
+
+address <- houses %>%
+      html_node(".zsg-photo-card-address") %>%
+      html_text()
+
+address
+str(address)

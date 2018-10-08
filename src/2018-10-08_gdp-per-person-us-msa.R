59997313011e4bd00c9c366a8b4e11543593753d
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2018-10-08 14:38:04 -0700

    graphs of gdp per pop in MSAs

--- src/2018-10-08_gdp-per-person-us-msa.R
+++ src/2018-10-08_gdp-per-person-us-msa.R
@@ -59,3 +59,62 @@ df3.gdp.and.pop <- df1.gdp %>%
                 factor)
 
 str(df3.gdp.and.pop)
+
+
+
+# 4) Graph the relationship: -------
+p1.gdp.per.person <- df3.gdp.and.pop %>% 
+      ggplot(aes(x = pop2017, 
+                 y = gdp_2017)) + 
+      geom_point(alpha = 0.5)  + 
+      geom_smooth(col = "dodgerblue4", 
+                  se = FALSE) +
+      geom_smooth(method = "lm", 
+                  colour = "red", 
+                  se = FALSE) + 
+      
+      theme_classic(); p1.gdp.per.person
+
+
+# we can't see most of the points; let's 
+# zoom in on values under 90th percentile: 
+
+p1.1.gdp.per.person.zoom <- p1.gdp.per.person + 
+      
+      coord_cartesian(xlim = c(0, quantile(df3.gdp.and.pop$pop2017, 
+                                           .90, 
+                                           na.rm = TRUE)), 
+                      ylim = c(0, quantile(df3.gdp.and.pop$gdp_2017, 
+                                           .90, 
+                                           na.rm = TRUE))) +
+      
+      theme_classic(); p1.1.gdp.per.person.zoom
+
+
+
+
+p2.logged.x <- p1.gdp.per.person + 
+      scale_x_log10(limits = c(1, 1e8), 
+                    breaks = c(1, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8), 
+                    labels = c("1", "10", "100", "1000", "10,000", 
+                               "100,000", "1,000,000", "10,000,000", 
+                               "100,000,000")); p2.logged.x
+    
+
+
+p3.logged.both.axis <- p2.logged.x + 
+      scale_y_log10(); p3.logged.both.axis
+
+
+
+# 4) Select random LSA in bottom and top decile of GDP -------
+
+
+
+# 5) Use GDP per cap to estimate unknown msa GDP: ------
+
+
+# 6) Use log-log linear relationship to estimate unknown msa GDP: ------
+
+
+
